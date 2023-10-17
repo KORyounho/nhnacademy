bf10c1fa089688513f3eeb6aab13073a2a47b3d4
dordyd <dordyd@naver.com>
2023-10-17 21:20:35 +0900

    ,

---
+++ introduction_to_programming_using_java/chapter2/example/HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        // subroutine call statement의 예시
+        // built-in subroutine을 사용중임.
+
+
+    }
+}
